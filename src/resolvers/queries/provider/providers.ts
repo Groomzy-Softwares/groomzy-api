7596af29adad22b00c3f54e3df648cfd81b61cc7
Sifiso <sifisomyeza@blackswan.com>
2021-06-17 22:30:35 +0200

    added more schema types, modified providers queries to match the updated schema structure

--- src/resolvers/queries/provider/providers.ts
+++ src/resolvers/queries/provider/providers.ts
@@ -5,6 +5,23 @@ export const providersQuery = async (_: any, __: any, ctx: IContext) => {
     return ctx.prisma.provider.findMany({
       include: {
         address: true,
+        dayTimes: {
+          include: {
+            day: true,
+            time: true,
+          },
+        },
+        staffs: true,
+        bookings: {
+          include: {
+            rating: true,
+          },
+        },
+        serviceProviderCategories: {
+          select: {
+            category: true,
+          },
+        },
       },
     });
   } catch (error) {
